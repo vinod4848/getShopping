16a6ee87fb34f6ae18ba8e6cc6e6f096ca68fb54
vinod4848 <vinodku4848@gmail.com>
2023-07-07 16:10:05 +0530

    remove commit code and fixce ui home page and Signup

--- src/pages/Resetpassword.js
+++ src/pages/Resetpassword.js
@@ -1,7 +1,6 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
-import { Link } from "react-router-dom";
 import Container from "../components/Container";
 import CustomInput from "../components/CustomInput";
 const Resetpassword = () => {
